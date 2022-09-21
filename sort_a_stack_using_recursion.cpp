195d934db844a9e150920f80351f53b02b291e7f
BhavyaGumber <79220494+BhavyaGumber@users.noreply.github.com>
2022-09-21 19:00:15 +0530

    Create sort_a_stack_using_recursion.cpp

---
+++ sort_a_stack_using_recursion.cpp
@@ -0,0 +1,24 @@
+void insert_at_correct_position(int x, stack<int> &s){
+    if(s.size()==0 || s.top()<x){
+        s.push(x);
+    }
+    else{
+        int a = s.top();
+        s.pop();
+        insert_at_correct_position(x,s);
+        s.push(a);
+    }
+}
+void reverse (stack<int> &s){
+    if(s.size()>0){
+        int x = s.top();
+        s.pop();
+        reverse(s);
+        insert_at_correct_position(x,s);
+    }
+}
+void SortedStack :: sort()
+{
+   //Your code here
+   reverse(s);
+}
