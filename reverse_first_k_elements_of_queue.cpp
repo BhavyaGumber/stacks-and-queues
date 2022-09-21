cc712e6295ab9ddb733d4dd9278beba49398682f
BhavyaGumber <79220494+BhavyaGumber@users.noreply.github.com>
2022-09-21 15:03:55 +0530

    Create reverse_first_k_elements_of_queue.cpp

---
+++ reverse_first_k_elements_of_queue.cpp
@@ -0,0 +1,20 @@
+queue<int> modifyQueue(queue<int> q, int k) {
+     stack<int> s;
+    for(int i=1; i<=k;i++){
+        s.push(q.front());
+        q.pop();
+    }
+    while(!s.empty()){
+        q.push(s.top());
+        s.pop();
+    }
+    int n = q.size();
+    for(int i=1;i<=n-k;i++){
+        int x=q.front();
+       // q.push(q.front());
+        q.pop();
+        q.push(x);
+    }
+    return q;
+    // add code here.
+}
